bd00fe41f2d1fb19ff2fa23d794bc49b88fb09f7
Ian-Ivano <paulomkali.jnr@gmail.com>
2025-10-26 20:13:20 +0300

    fix syntax

--- 6pGVX/keymap.c
+++ 6pGVX/keymap.c
@@ -310,14 +310,13 @@ switch (keycode) {
       }
       break;
     case M_AIR:
+        if(get_repeat_key_count()<-1){
+        SEND_STRING(SS_TAP(X_LEFT) SS_DELAY(100) SS_TAP(X_LEFT) SS_DELAY(100));
+      }
       if (get_repeat_key_count()<0){
         if(record->event.pressed){
         SEND_STRING("/s");
-        return false;
-        if(get_repeat_key_count()<-1){
-        SEND_STRING(SS_TAP(X_LEFT) SS_DELAY(100) SS_TAP(X_LEFT) SS_DELAY(100));
         }
-      }
       return false;
       }
       break;
