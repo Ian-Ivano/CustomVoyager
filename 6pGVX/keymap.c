f10f6dd70dd69044cfab08612e37b6e569556f93
Ian-Ivano <paulomkali.jnr@gmail.com>
2025-10-26 20:01:17 +0300

    trial

--- 6pGVX/keymap.c
+++ 6pGVX/keymap.c
@@ -313,6 +313,7 @@ switch (keycode) {
       if (get_repeat_key_count()<0){
         if(record->event.pressed){
         SEND_STRING("/s");
+        return false;
         if(get_repeat_key_count()<-1){
         SEND_STRING(SS_TAP(X_LEFT) SS_DELAY(100) SS_TAP(X_LEFT) SS_DELAY(100));
         }
